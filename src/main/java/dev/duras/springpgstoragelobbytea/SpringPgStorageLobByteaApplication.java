e17a9c338ab4a5fb21db037a2608dd75a4cf85bc
batuhan.duras <batugane@gmail.com>
2025-01-12 19:16:58 +0300

    initial commit

---
+++ src/main/java/dev/duras/springpgstoragelobbytea/SpringPgStorageLobByteaApplication.java
@@ -0,0 +1,13 @@
+package dev.duras.springpgstoragelobbytea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPgStorageLobByteaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPgStorageLobByteaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/duras/springpgstoragelobbytea/SpringPgStorageLobByteaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.duras.springpgstoragelobbytea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPgStorageLobByteaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
